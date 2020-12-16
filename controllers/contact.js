a91655bc5c128e4326569ac872ff08b5888bd1b2
Willian <williamhernandez450@gmail.com>
2020-12-16 15:00:05 -0600

    Clase 7, uso de middleware y metodo POST. se implemento el middleware o pauete body-parser en el proyecto

--- controllers/contact.js
+++ controllers/contact.js
@@ -1,7 +1,11 @@
+
 //aca toda la logica de la renderizacion de esta pagina
 export const renderContact = (req, res) => {
     res.render( "contact.ejs", { path:"Contact" } );
 }
 
+const contact = ( req, res ) => {
+    res.send( req.body );
+}
 // segunda forma de exportar
-export default { renderContact }
+export default { renderContact, contact }
--- routes/contact.js
+++ routes/contact.js
@@ -2,11 +2,14 @@
 import express from 'express'
 // importamos el controlador
 import controller from '../controllers/contact.js'
+import bodyParser from 'body-parser'
 //esta es una ruta va recibir todas las acciones
 const router = express.Router();
-
+router.use( bodyParser.urlencoded ({ extended: true}));
 //el primer get sera para enviar la primera pagina que se quiere mostrar
 //pueden enviarse diferentes metodos
 router.get('/', controller.renderContact);
+
+router.post('/', controller.contact);
 
 export default router
